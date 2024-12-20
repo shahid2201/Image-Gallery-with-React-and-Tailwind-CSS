78bf35d750efa1eef27e1ae4017dc21c13f865be
shahid2201 <sheikhshahid2201@gmail.com>
2024-12-19 19:47:58 -0500

    Image Component

--- tailwind.js
+++ tailwind.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [ './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths based on your project structure ],
+  ],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
