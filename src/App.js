058a4d717faf1f1d8af3cb8eecb7b032901d9007
shahid2201 <sheikhshahid2201@gmail.com>
2024-12-19 21:02:30 -0500

    Search Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import autoprefixer from "autoprefixer";
 import React, {useState, useEffect} from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -11,14 +12,17 @@ function App() {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
       .then(data=> {
-        setImages(data.hits);
+        setImages(data.hits); 
         setIsLoading(false);
       })
       .catch(err => console.log(err));
-    }, [])
+    }, [term])
 
   return (
     <div className="container mx-auto">
+      <ImageSearch searchTerm = {(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+      
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image}/>
